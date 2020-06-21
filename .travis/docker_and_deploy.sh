69f35f459bec4ae1a716eb53a591e99cb39f9889
siamofuori <fische>
2020-06-21 13:42:18 -0300

    CI

---
+++ .travis/docker_and_deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    echo "*** Building Docker Containers ***"
+    docker build -f docker/Dockerfile -t unqdifi/covid-front-node .
+
+    echo "*** Pushing Docker Containers ***"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker push unqdifi/covid-front-node:latest
+
+    echo "*** Deploying on Server ***"
+    echo "$SERVER_IP ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
+    ssh $SERVER_USERNAME@$SERVER_IP "/home/$SERVER_USERNAME/difi-covid-deploy.sh"
+else
+    echo "Not deploying, since this branch isn't master or it's a Pull Request."
+fi

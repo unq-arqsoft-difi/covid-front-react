67f9152dede91cc4b0f0299b8ef6031ca1097524
fischettij <fischettij@users.noreply.github.com>
2020-06-22 19:34:37 -0300

    Cambio en el paht de dockerhub

--- .travis/docker_and_deploy.sh
+++ .travis/docker_and_deploy.sh
@@ -2,11 +2,11 @@
 
 if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo "*** Building Docker Containers ***"
-    docker build -f docker/Dockerfile -t unqdifi/covid-front-node .
+    docker build -f docker/Dockerfile -t unqdifi/covid-front-react .
 
     echo "*** Pushing Docker Containers ***"
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker push unqdifi/covid-front-node:latest
+    docker push unqdifi/covid-front-react:latest
 
     echo "*** Deploying on Server ***"
     echo "$SERVER_IP ecdsa-sha2-nistp256 $SERVER_KEY" >> ~/.ssh/known_hosts
